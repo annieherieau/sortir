675edb5b6949498fa2408d15908500165196339c
annieherieau <annie.herieau@gmail.com>
2025-10-20 15:48:34 +0200

    install bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,11 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+  /*
+  * Welcome to your app's main JavaScript file!/n
+  *
+  * This file will be included onto the page via the importmap() Twig function,
+  * which should already be in your base.html.twig.
+  */
+  import 'bootstrap';  // Déclaration du JS de Bootstrap
+  import 'bootstrap/dist/css/bootstrap.min.css';  // Déclaration du CSS de Bootstrap
+  import './styles/app.css';
+  console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
